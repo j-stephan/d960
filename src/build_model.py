cb08fe2743e44dfa5f23d9a3ea347a674be97459
Jan Stephan <jan.stephan.dd@gmail.com>
2018-08-16 00:48:53 +0200

    Working for word length = 1

--- src/build_model.py
+++ src/build_model.py
@@ -5,12 +5,10 @@ import sys
 
 from keras import backend as K
 from keras.layers import Activation, Add, BatchNormalization, Bidirectional
-from keras.layers import Conv2D, Dense, Input, LSTM, MaxPooling2D, Reshape
-from keras.layers import TimeDistributed
+from keras.layers import Conv2D, Dense, Input, Lambda, LSTM, MaxPooling2D
+from keras.layers import Reshape, TimeDistributed
 from keras.models import Model, Sequential
-
-from sklearn.preprocessing import LabelBinarizer
-from sklearn.model_selection import train_test_split
+from keras.utils import to_categorical
 
 data_input = "wl1"
 
@@ -20,6 +18,16 @@ image_paths = [data_input + "/{0}".format(f)
 
 data = []
 labels = []
+
+# Shamelessly stolen from Keras' image_ocr.py example
+def ctc_lambda_func(args):
+    y_pred, labels, input_length, label_length = args
+    print(y_pred)
+    print(labels)
+    print(input_length)
+    print(label_length)
+    y_pred = y_pred[:, 2:, :]
+    return K.ctc_batch_cost(labels, y_pred, input_length, label_length)
 
 # Load images, convert to greyscale
 for image_path in image_paths:
@@ -46,112 +54,114 @@ for image_path in image_paths:
     data.append(grey)
     labels.append(label)
 
-alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-            'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-# Create numpy arrays
-data = np.array(data)
-labels = np.array(labels)
-
-# Create training and test data
-(X_train, X_test, Y_train, Y_test) = train_test_split(data, labels,
-                                        test_size=0.25, random_state=0)
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+# Create mapping from char to int
+char_to_int = dict((c, i) for i, c in enumerate(alphabet))
+int_to_char = dict((i, c) for i, c in enumerate(alphabet))
 
 # Convert labels into one-hot encodings for Keras
-lb = LabelBinarizer().fit(alphabet)
-Y_train = lb.transform(Y_train)
-Y_test = lb.transform(Y_test)
+labels[:] = [[char_to_int[c] for c in lb] for lb in labels]
+labels = to_categorical(labels)
+print(labels)
 
-X_train = X_train.reshape(X_train.shape[0], 64, 64, 1)
-X_test = X_test.reshape(X_test.shape[0], 64, 64, 1)
+# Create numpy arrays
+X = np.array(data)
+Y = np.array(labels)
 
+X = X.reshape(X.shape[0], 64, 64, 1)
 
 model = Sequential()
 
+# Input layer
+input_data = Input(name="input_data", shape = (64, 64, 1), dtype = "float32")
+
 # First convolutional layer
-print("Adding first Conv2D")
-model.add(Conv2D(filters = 64, kernel_size = (3, 3), padding = "same",
-                 activation = "relu", input_shape = (64, 64, 1)))
-model.add(MaxPooling2D(pool_size = (2, 2), strides = (2, 2)))
+conv = Conv2D(filters = 64, kernel_size = (3, 3), padding = "same",
+              activation = "relu")(input_data)
+conv = MaxPooling2D(pool_size = (2, 2), strides = (2, 2))(conv)
 
 # Second convolutional layer
-print("Adding second Conv2D")
-model.add(Conv2D(filters = 128, kernel_size = (3, 3), padding = "same",
-                 activation = "relu"))
-model.add(MaxPooling2D(pool_size = (2, 2), strides = (2, 2)))
+conv = Conv2D(filters = 128, kernel_size = (3, 3), padding = "same",
+              activation = "relu")(conv)
+conv = MaxPooling2D(pool_size = (2, 2), strides = (2, 2))(conv)
 
 # Third convolutional layer
-print("Adding third Conv2D")
-model.add(Conv2D(filters = 256, kernel_size = (3, 3), padding = "same",
-                 activation = "relu"))
+conv = Conv2D(filters = 256, kernel_size = (3, 3), padding = "same",
+              activation = "relu")(conv)
 
 # Fourth convolutional layer
-print("Adding fourth Conv2D")
-model.add(Conv2D(filters = 256, kernel_size = (3, 3), padding = "same",
-                 activation = "relu"))
-model.add(MaxPooling2D(pool_size = (1, 2), strides = (2, 2)))
+conv = Conv2D(filters = 256, kernel_size = (3, 3), padding = "same",
+              activation = "relu")(conv)
+conv = MaxPooling2D(pool_size = (1, 2), strides = (2, 2))(conv)
 
 # Fifth convolutional layer
-print("Adding fifth Conv2D")
-model.add(Conv2D(filters = 512, kernel_size = (3, 3), padding = "same",
-                 activation = "relu"))
+conv = Conv2D(filters = 512, kernel_size = (3, 3), padding = "same",
+              activation = "relu")(conv)
 
 # First normalization layer
-print("Adding first normalization layer")
-model.add(BatchNormalization())
+conv = BatchNormalization()(conv)
 
 # Sixth convolutional layer
-print("Adding sixth Conv2D")
-model.add(Conv2D(filters = 512, kernel_size = (3, 3), padding = "same",
-                 activation = "relu"))
+conv = Conv2D(filters = 512, kernel_size = (3, 3), padding = "same",
+              activation = "relu")(conv)
 
 # Second normalization layer
-print("Adding second normalization layer")
-model.add(BatchNormalization())
-model.add(MaxPooling2D(pool_size = (1, 2), strides = (2, 2)))
+conv = BatchNormalization()(conv)
+conv = MaxPooling2D(pool_size = (1, 2), strides = (2, 2))(conv)
 
 # Seventh convolutional layer
-print("Adding seventh Conv2D")
-model.add(Conv2D(filters = 512, kernel_size = (2, 2), padding = "valid",
-                 activation = "relu"))
-
+conv = Conv2D(filters = 512, kernel_size = (2, 2), padding = "valid",
+              activation = "relu")(conv)
 
 # Reshape layer
-print("Adding reshape")
-model.add(Reshape((4, 1152)))
+conv = Reshape((16, 288))(conv)
 
-# LSTM layer
-print("Adding bidirectional LSTM layer")
-model.add(Bidirectional(LSTM(units = 512, return_sequences = True),
-                        merge_mode = "sum"))
+# Bidirectional LSTM layer
+lstm = Bidirectional(LSTM(units = 512, return_sequences = True),
+                     merge_mode = "sum")(conv)
 
 # transform RNN output to character activation
-# 52 output units: 52 letters -> (upper/lowercase)
-print("Adding activation")
-model.add(Dense(52, kernel_initializer = "he_normal",
-                activation = "softmax"))
+# 23 output units: 52 letters (uppercase/lowercase)
+prediction = Dense(52, kernel_initializer = "he_normal",
+                   activation = "softmax")(lstm)
 
-model.summary()
+Model(inputs=input_data, outputs = prediction).summary()
+
+# CTC loss
+print("CTC loss")
+labels = Input(name = "labels", shape = (52, ), dtype = "float32")
+input_length = Input(name = "input_length", shape = [1], dtype = "int64")
+label_length = Input(name = "label_length", shape = [1], dtype = "int64")
+loss_out = Lambda(ctc_lambda_func, output_shape = (1,), name = "CTC")(
+                  [prediction, labels, input_length, label_length])
+
+print("Defining model")
+model = Model(inputs = [input_data, labels, input_length, label_length],
+              outputs = loss_out)
 
 # Compile
 print("Compiling model")
-model.compile(loss="categorical_crossentropy", optimizer="adam",
-              metrics=["accuracy"])
+model.compile(loss={"CTC": lambda y_true, y_pred: y_pred},
+              optimizer="adam", metrics=["accuracy"])
 
 # Train
 print("Fitting model")
-model.fit(X_train, Y_train,
-          validation_data = (X_test, Y_test),
+input_length_arr = [14 for x in range(0, X.shape[0])]
+il_arr = np.array(input_length_arr)
+
+label_length_arr = [1 for y in range(0, Y.shape[0])]
+ll_arr = np.array(label_length_arr)
+
+model.fit(x = [X, Y, il_arr, ll_arr], y = Y,
+          validation_split = 0.25,
           batch_size = 32,
           epochs = 20, verbose = 1)
 
 # Evaluate
-print("Evaluating model")
-score = model.evaluate(X_test, Y_test, verbose = 1)
-print(model.metrics_names)
-print(score)
+#print("Evaluating model")
+#score = model.evaluate(X_test, Y_test, verbose = 1)
+#print(model.metrics_names)
+#print(score)
 
 # Save
 print("Saving model")
